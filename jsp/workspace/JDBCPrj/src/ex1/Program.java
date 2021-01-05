b67fc87016f6c6f32761596f8f5ed4133fdc421f
dragonbead95 <dragonbead95@naver.com>
2021-01-05 15:58:23 +0900

    done:)

--- jsp/workspace/JDBCPrj/src/ex1/Program.java
+++ jsp/workspace/JDBCPrj/src/ex1/Program.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
 
@@ -13,18 +14,25 @@ public class Program {
 		// 드라이버를 로드하게 되면 메모리 상에 잡히게 된다.
 		try {
 			String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-			String sql = "select * from notice";
+			String sql = "select * from notice where hit>10";
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection con = DriverManager.getConnection(url,"newlec","newlec"); // 드라이버 매니저를 통해서 연결 객체 생성
 			Statement st = con.createStatement(); // 실행 도구 생성
 			ResultSet rs = st.executeQuery(sql); // 쿼리 실행후 결과를 얻어서 fetch해올수 있게함
 			
 			
-			if(rs.next()) {
+			while(rs.next())
+			{
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String writerId = rs.getString("writer_id");
+				Date regDate = rs.getDate("regdate");
+				String content = rs.getString("content");
+				int hit = rs.getInt("hit");
+
+				System.out.printf("id :%d, title:%s, writerId:%s, regDate:%s, content:%s, hit:%d\n", id,title,writerId,regDate,content,hit);
 				
-				String name = rs.getString("title");
-				System.out.printf("NAME:%s\n",name);	
-			};
+			}
 			
 			
 			
