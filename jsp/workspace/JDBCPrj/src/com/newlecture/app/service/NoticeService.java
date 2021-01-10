c13a3ef1ea7700305a4628d2a3dac3ad3ba6ad59
dragonbead95 <dragonbead95@naver.com>
2021-01-10 12:06:23 +0900

    done:)

---
+++ jsp/workspace/JDBCPrj/src/com/newlecture/app/service/NoticeService.java
@@ -0,0 +1,147 @@
+package com.newlecture.app.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.newlecture.app.entity.Notice;
+
+public class NoticeService {
+	private String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	private String uid = "newlec";
+	private String pwd = "newlec";
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	
+	public List<Notice> getList()
+	{
+		List<Notice> list = new ArrayList<Notice>();
+		try {
+			String sql = "select * from notice";
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,uid,pwd); // 드라이버 매니저를 통해서 연결 객체 생성
+			Statement st = con.createStatement(); // 실행 도구 생성
+			ResultSet rs = st.executeQuery(sql); // 쿼리 실행후 결과를 얻어서 fetch해올수 있게함
+		
+			
+			
+			while(rs.next())
+			{
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String writerId = rs.getString("writer_id");
+				Date regDate = rs.getDate("regdate");
+				String content = rs.getString("content");
+				int hit = rs.getInt("hit");
+				String files = rs.getString("files");
+				Notice notice = new Notice(id, title, writerId, regDate, content, hit,files);
+				
+				list.add(notice);
+			}
+		
+			rs.close();
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public int insert(Notice notice)
+	{
+		String title = notice.getTitle();
+		String writer_id = notice.getWriterId();
+		String content = notice.getContent();
+		String files = notice.getFiles();
+		int result = 0;
+		try {
+			String sql = "INSERT INTO notice (title, writer_id, content, files) VALUES (?,?,?,?)";
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,uid,pwd); // 드라이버 매니저를 통해서 연결 객체 생성
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, title); // id:0, title:1 ...
+			st.setString(2, writer_id);
+			st.setString(3, content);
+			st.setString(4, files);
+			
+			result = st.executeUpdate();
+					
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int update(Notice notice)
+	{
+		String title = notice.getTitle();
+		String writer_id = notice.getWriterId();
+		String content = notice.getContent();
+		String files = notice.getFiles();
+		int id = notice.getId();
+		int result = 0;
+		try {
+			String sql = "update notice set title=?, content=?, files=? where id=?";
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,uid,pwd); // 드라이버 매니저를 통해서 연결 객체 생성
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, title); // id:0, title:1 ...
+			st.setString(2, content);
+			st.setString(3, files);
+			st.setInt(4, id);
+			
+			result = st.executeUpdate();
+			
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int delete(Notice notice)
+	{
+		String title = notice.getTitle();
+		String writer_id = notice.getWriterId();
+		String content = notice.getContent();
+		String files = notice.getFiles();
+		int id = notice.getId();
+		int result = 0;
+		try {
+			String sql = "delete notice where id = ?";
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url,uid,pwd); // 드라이버 매니저를 통해서 연결 객체 생성
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+			
+			result = st.executeUpdate();
+			
+			
+			
+			st.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
